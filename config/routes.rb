c8844067b14f5be5ecac5b14701864ea58cce418
Brian Liu <brian.b.liu@gmail.com>
2020-12-17 14:16:37 -0700

    Add routes for nested search controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,18 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :items
+      resources :items, only: [:index]
+      get '/items/find', to: "items/search#show"
+      get '/items/find_all', to: "items/search#index"
+      resources :items, except: [:index]
+
       get '/items/:id/merchants', to: "items/merchants#show"
-      resources :merchants
+
+      resources :merchants, only: [:index]
+      get '/merchants/find', to: "merchants/search#show"
+      get '/merchants/find_all', to: "merchants/search#index"
+      resources :merchants, except: [:index]
+
       get '/merchants/:id/items', to: "merchants/items#index"
     end
   end
