5f16810d90f684f16855ace13eaf5c583f0f62b7
Brian Liu <brian.b.liu@gmail.com>
2020-12-18 03:09:43 -0700

    Create helper method for items and merchants search controllers

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -2,15 +2,13 @@ class Api::V1::Items::SearchController < ApplicationController
   def index
     item_params.each do |key, value|
       if key == "unit_price"
-        @items = Item.where("#{key} = ?", "#{value.to_f}")
+        @items = search_price(key, value)
       elsif key == "merchant_id"
-        @items = Item.where("#{key} = ?", "#{value.to_i}")
+        @items = search_merchant_id(key, value)
       elsif key == "name" || key == "description"
-        @items = Item.where("lower(#{key}) like ?", "%#{value.downcase}%")
+        @items = search_name_desc(key, value)
       else key == "created_at" || key == "updated_at"
-        @items = Item.all.each do |item|
-          "item.#{key}.to_date" == "#{value.to_date}"
-        end
+        @items = search_date(key, value)
       end
     end
     render json: ItemSerializer.new(@items)
@@ -19,15 +17,13 @@ class Api::V1::Items::SearchController < ApplicationController
   def show
     item_params.each do |key, value|
       if key == "unit_price"
-        @items = Item.where("#{key} = ?", "#{value.to_f}")
+        @items = search_price(key, value)
       elsif key == "merchant_id"
         @items = Item.where("#{key} = ?", "#{value.to_i}")
       elsif key == "name" || key == "description"
-        @items = Item.where("lower(#{key}) like ?", "%#{value.downcase}%")
+        @items = search_name_desc(key, value)
       else key == "created_at" || key == "updated_at"
-        @items = Item.all.each do |item|
-          "item.#{key}.to_date" == "#{value.to_date}"
-        end
+        @items = search_date(key, value)
       end
     end
     render json: ItemSerializer.new(@items[0])
@@ -37,5 +33,23 @@ class Api::V1::Items::SearchController < ApplicationController
 
   def item_params
     params.permit(:name, :description, :unit_price, :merchant_id, :created_at, :updated_at)
+  end
+
+  def search_price(key, value)
+    Item.where("#{key} = ?", "#{value.to_f}")
+  end
+
+  def search_merchant_id(key, value)
+    Item.where("#{key} = ?", "#{value.to_i}")
+  end
+
+  def search_name_desc(key, value)
+    Item.where("lower(#{key}) like ?", "%#{value.downcase}%")
+  end
+
+  def search_date(key, value)
+    Item.all.each do |item|
+      "item.#{key}.to_date" == "#{value.to_date}"
+    end
   end
 end
