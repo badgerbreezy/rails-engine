702512de8de4e54177e806153bd072c2a3c68aa5
Brian Liu <brian.b.liu@gmail.com>
2020-12-18 01:02:10 -0700

    Revise destroy action

--- app/controllers/api/v1/items/merchants_controller.rb
+++ app/controllers/api/v1/items/merchants_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::Items::MerchantsController < ApplicationController
   def show
     item = Item.find(params[:id])
-    render json: MerchantSerializer.new(Merchant.where(id: item.merchant_id))
+    render json: MerchantSerializer.new(item.merchant)
   end
 end
