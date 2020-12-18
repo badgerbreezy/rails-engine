16ddf64208731b50911f292c127d362af3780b67
Brian Liu <brian.b.liu@gmail.com>
2020-12-18 01:00:21 -0700

    Create AR queries for merchant

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,5 +2,45 @@ class Merchant < ApplicationRecord
   validates :name, presence: true
 
   has_many :items, dependent: :destroy
-  has_many :invoices
+  has_many :invoices, dependent: :destroy
+
+  def self.most_revenue(limit)
+    select("merchants.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue")
+      .joins(invoices: [:invoice_items, :transactions])
+      .merge(Transaction.successful)
+      .where(invoices: {status: 'shipped'})
+      .group(:id)
+      .order("revenue desc")
+      .limit(limit)
+  end
+
+  def self.merchant_revenue(id)
+    select("merchants.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue")
+    .joins(invoices: [:invoice_items, :transactions])
+    .merge(Transaction.successful)
+    .where(invoices: {status: 'shipped'})
+    .where(id: id)
+    .group(:id)
+    .first
+  end
+
+  def self.most_items(quantity)
+    select("merchants.*, SUM(invoice_items.quantity) AS revenue")
+      .joins(invoices: [:invoice_items, :transactions])
+      .merge(Transaction.successful)
+      .where(invoices: {status: 'shipped'})
+      .group(:id)
+      .order("revenue desc")
+      .limit(quantity)
+  end
+
+  def self.revenue_across_dates(start, ending)
+    start_date = start.to_date.beginning_of_day
+    end_date = ending.to_date.end_of_day
+    Invoice.select("SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue")
+      .joins(:invoice_items, :transactions)
+      .merge(Transaction.successful)
+      .where(invoices: { status: 'shipped' })
+      .where(invoices: { created_at: start_date..end_date })[0]
+  end
 end
