01e621e03a392dfbe614d25e7daacf7f0f8f5d40
Brian Liu <brian.b.liu@gmail.com>
2020-12-18 03:12:28 -0700

    Revise routes to be nested correctly and restful

--- config/routes.rb
+++ config/routes.rb
@@ -2,27 +2,22 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :items, only: [:index]
+      namespace :items do
+        get '/find', to: "search#show"
+        get '/find_all', to: "search#index"
+        get '/:id/merchants', to: "merchants#show"
+      end
+      resources :items
 
-      get '/items/find', to: "items/search#show"
-      get '/items/find_all', to: "items/search#index"
-
-      resources :items, except: [:index]
-
-      get '/items/:id/merchants', to: "items/merchants#show"
-
-      resources :merchants, only: [:index]
-
-      get '/merchants/find', to: "merchants/search#show"
-      get '/merchants/find_all', to: "merchants/search#index"
-
-      get '/merchants/most_revenue', to: 'merchants/business_intelligence#most_revenue'
-      get '/merchants/:id/revenue', to: 'merchants/business_intelligence#merchant_revenue'
-      get '/merchants/most_items', to: "merchants/business_intelligence#most_items"
-
-      resources :merchants, except: [:index]
-
-      get '/merchants/:id/items', to: "merchants/items#index"
+      namespace :merchants do
+        get '/find', to: "search#show"
+        get '/find_all', to: "search#index"
+        get '/most_revenue', to: 'business_intelligence#most_revenue'
+        get '/:id/revenue', to: 'business_intelligence#merchant_revenue'
+        get '/most_items', to: "business_intelligence#most_items"
+        get '/:id/items', to: "items#index"
+      end
+      resources :merchants
 
       get '/revenue', to: "merchants/business_intelligence#revenue_across_dates"
     end
